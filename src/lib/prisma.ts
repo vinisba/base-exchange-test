b2033c62d90ef4cafc6c4d560ea120c9646a4632
Vinícius Borba <vinisba@gmail.com>
2026-03-15 20:25:01 -0300

    build: update prisma envs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = process.env.POSTGRES_URL;
 
 if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set");
+  throw new Error("POSTGRES_URL is not set");
 }
 
 const adapter = new PrismaPg({ connectionString: databaseUrl });
